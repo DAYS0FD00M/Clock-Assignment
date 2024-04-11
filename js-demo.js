fe7e08048f868833bca0db5baac1972b2fecd513
DAYS0FD00M <165326683+DAYS0FD00M@users.noreply.github.com>
2024-04-11 13:39:13 -0700

    Add files via upload

---
+++ js-demo.js
@@ -0,0 +1,27 @@
+﻿/*
+   JS Activity Clock
+   Name: Alex Day
+   Date: 03/28/2024  
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
+runClock();
+   setInterval("runclock()", 1000);
+
+function runClock(){
+   var thisDay = new Date();
+   var thisDate = thisDay.toLocaleDateString();
+   var thisDayNum = thisDay.getDay();
+   var thisWeekday = getWeekday(thisDayNum);
+   var thisTime = thisDay.toLocaleTimeString();
+
+   document.getElementById("date").textContent = thisDate;
+   document.getElementById("wday").textContent = thisWeekday;
+   document.getElementById("time").textContent = thisTime;
+   }
+   function getWeekday(dayNum){
+      var wDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      return wDays [dayNum];
+   }
